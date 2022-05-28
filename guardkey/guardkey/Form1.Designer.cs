ce0f185271c275ac61f2524fdbaf9b74cd7abb92
Sebastian Wallace <sebastian.wallace.uk@gmail.com>
2022-05-28 08:59:50 +0100

    initi commit

---
+++ guardkey/guardkey/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace guardkey
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this._username = new System.Windows.Forms.TextBox();
+            this._password = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this._guardKey = new System.Windows.Forms.TextBox();
+            this._generate_label = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(149, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Username";
+            // 
+            // _username
+            // 
+            this._username.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._username.Location = new System.Drawing.Point(92, 23);
+            this._username.Name = "_username";
+            this._username.Size = new System.Drawing.Size(224, 25);
+            this._username.TabIndex = 1;
+            this._username.TextChanged += new System.EventHandler(this._username_TextChanged);
+            // 
+            // _password
+            // 
+            this._password.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._password.Location = new System.Drawing.Point(92, 54);
+            this._password.Name = "_password";
+            this._password.PasswordChar = '*';
+            this._password.Size = new System.Drawing.Size(224, 25);
+            this._password.TabIndex = 2;
+            this._password.TextChanged += new System.EventHandler(this._password_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(22, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password";
+            // 
+            // _guardKey
+            // 
+            this._guardKey.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._guardKey.Location = new System.Drawing.Point(12, 138);
+            this._guardKey.Multiline = true;
+            this._guardKey.Name = "_guardKey";
+            this._guardKey.ReadOnly = true;
+            this._guardKey.Size = new System.Drawing.Size(304, 136);
+            this._guardKey.TabIndex = 6;
+            // 
+            // _generate_label
+            // 
+            this._generate_label.AutoSize = true;
+            this._generate_label.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._generate_label.Location = new System.Drawing.Point(12, 93);
+            this._generate_label.Name = "_generate_label";
+            this._generate_label.Size = new System.Drawing.Size(134, 17);
+            this._generate_label.TabIndex = 7;
+            this._generate_label.Text = "Generated Guard Key";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(175, 290);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 39);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "CLOSE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(48, 290);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 41);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "GENERATE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 348);
+            this.ControlBox = false;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this._generate_label);
+            this.Controls.Add(this._guardKey);
+            this.Controls.Add(this._password);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this._username);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(344, 387);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(344, 387);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox _username;
+        private System.Windows.Forms.TextBox _password;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox _guardKey;
+        private System.Windows.Forms.Label _generate_label;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ guardkey/guardkey/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace guardkey
+{
+    public partial class Form1 : Form
+    {
+        private string appId = "27116";
+        private string appKey = "";
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "App ID " + appId;
+        }
+
+        public void generateGuardKey()
+        {
+            String sDate = DateTime.Now.ToString();
+            DateTime datevalue = (Convert.ToDateTime(sDate.ToString()));
+
+            int minutes = Convert.ToInt32(datevalue.Minute.ToString());
+
+            string remaining = Convert.ToString(60 - minutes);
+
+            String hr = datevalue.Hour.ToString();
+            String dy = datevalue.Day.ToString();
+            String mn = datevalue.Month.ToString();
+            String yy = datevalue.Year.ToString();
+            string computedHash = sha512(_username.Text) + sha512(_password.Text) + sha512(hr + dy + mn + yy) + sha512(appKey);
+
+            _guardKey.Text = sha512(computedHash);
+            System.Windows.Forms.Clipboard.SetText(_guardKey.Text);
+            _generate_label.Text = "Generated Guard Key (COPIED TO CLIPBOARD!)" + "\n" + "valid for " + remaining + " minutes";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private string sha512(string input)
+        {
+            var bytes = System.Text.Encoding.UTF8.GetBytes(input);
+            using (var hash = System.Security.Cryptography.SHA512.Create())
+            {
+                var hashedInputBytes = hash.ComputeHash(bytes);
+
+                // Convert to text
+                // StringBuilder Capacity is 128, because 512 bits / 8 bits in byte * 2 symbols for byte 
+                var hashedInputStringBuilder = new System.Text.StringBuilder(128);
+                foreach (var b in hashedInputBytes)
+                    hashedInputStringBuilder.Append(b.ToString("X2"));
+                return hashedInputStringBuilder.ToString();
+            }
+        }
+
+        private void _username_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void _password_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void _password_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                generateGuardKey();
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            generateGuardKey();
+        }
+    }
+}
